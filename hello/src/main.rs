c3a4ec0e7c11257adec4ad42dffa4ce206711c38
鲁江文 <jiangwen.lu@ujoin-tech.com>
2024-07-17 11:32:47 +0800

    demo-study

---
+++ hello/src/main.rs
@@ -0,0 +1,102 @@
+use std::fmt; // 导入 `fmt` 模块。
+
+// 定义一个包含单个 `Vec` 的结构体 `List`。
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // 使用元组的下标获取值，并创建一个 `vec` 的引用。
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // 使用 `v` 对 `vec` 进行迭代，并用 `count` 记录迭代次数。
+        for (count, v) in vec.iter().enumerate() {
+            // 对每个元素（第一个元素除外）加上逗号。
+            // 使用 `?` 或 `try!` 来返回错误。
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+
+        // 加上配对中括号，并返回一个 fmt::Result 值。
+        write!(f, "]")
+    }
+}
+
+// 推导 `Structure` 的 `fmt::Debug` 实现。
+// `Structure` 是一个包含单个 `i32` 的结构体。
+#[derive(Debug)]
+struct Structure(i32);
+
+// 将 `Structure` 放到结构体 `Deep` 中。然后使 `Deep` 也能够打印。
+#[derive(Debug)]
+struct Deep(Structure);
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // 美化打印
+    println!("{:#?}", peter);
+
+    // 使用 `{:?}` 打印和使用 `{}` 类似。
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+
+    // `Structure` 也可以打印！
+    println!("Now {:?} will print!", Structure(3));
+
+    // 使用 `derive` 的一个问题是不能控制输出的形式。
+    // 假如我只想展示一个 `7` 怎么办？
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    println!("Hello, world!");
+
+    // 用变量替换字符串有多种写法。
+    // 比如可以使用位置参数。
+    println!("{0}, this is {1}. {1}, this is {0}", 1, 2);
+
+    // 可以使用命名参数。
+    println!(
+        "{subject} {verb} {object}",
+        object = "the lazy dog",
+        subject = "the quick brown fox",
+        verb = "jumps over"
+    );
+
+    // 你可以按指定宽度来右对齐文本。
+    // 下面语句输出 "     1"，5 个空格后面连着 1。
+    println!("{number:>width$}", number = 1, width = 6);
+
+    // 你可以在数字左边补 0。下面语句输出 "000001"。
+    println!("{number:>0width$}", number = 1, width = 6);
+
+    // println! 会检查使用到的参数数量是否正确。
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // 改正 ^ 补上漏掉的参数："James"
+
+    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
+    // 下面语句无法运行。
+    // println!("This struct `{}` won't print...", Structure(3));
+    // 改正 ^ 注释掉此行。
+
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}", pi);
+}
